3466479bbe9bd50613b251ba5583cf7d8abe6653
jrphub <jyotiranjanpattnaik@gmail.com>
2022-08-14 10:08:43 +0530

    minor change

--- src/main/java/com/ls/java8/lambda/AintroToLambda/Lambda1.java
+++ src/main/java/com/ls/java8/lambda/AintroToLambda/Lambda1.java
@@ -8,7 +8,7 @@ public class Lambda1 {
         List<String> empList = Arrays.asList("Zubin", "Payal", "Ravi", "Deepak");
 
         //1st
-        Collections.sort(empList, new Comparator<String>() {
+        empList.sort(new Comparator<String>() {
             @Override
             public int compare(String emp1, String emp2) {
                 return emp1.compareToIgnoreCase(emp2);
@@ -17,14 +17,14 @@ public class Lambda1 {
         System.out.println("1 - " + empList);
 
         //2nd -In Java8, String class has compareToIgnoreCase and compareTo methods in built
-        Collections.sort(empList, (String emp1, String emp2) -> {
+        empList.sort((String emp1, String emp2) -> {
             return emp1.compareToIgnoreCase(emp2);
         });
 
         System.out.println("2 - " + empList);
 
         //3rd
-        Collections.sort(empList, (emp1, emp2) -> emp1.compareToIgnoreCase(emp2));
+        empList.sort((emp1, emp2) -> emp1.compareToIgnoreCase(emp2));
         System.out.println("3 - " + empList );
 
         //4th - List interface has sort method itself in Java 8
